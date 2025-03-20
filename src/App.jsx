0e3516c608f2d7b45531635abc4cb8666f3eb320
Diego <dasc2703@gmail.com>
2025-03-20 13:50:27 -0500

    Pagina de contacto transformada a React con exito!

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import Main from './layouts/Main/Main'
 import Home from './pages/Home/Home'
 import Register from './pages/Register/Register'
+import Contact from './pages/Contact/Contact'
 import './App.css'
 import './form.css'
 
@@ -13,6 +14,8 @@ function App() {
           <Route path='/' element={ <Main/> }>
             <Route index element={ <Home/> }/>
             <Route path='register' element={ <Register/> }/>
+            <Route path='contact' element={ <Contact/> }/>
+            <Route path='*' element={ <h1>404 Not Found</h1> }/>
           </Route>
         </Routes>
     </>
